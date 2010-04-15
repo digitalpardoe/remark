d5feea8cb78e992f7205af4e022cd5a862c0ce69
Alex Pardoe <digital.pardoe@gmail.com>
2010-04-15 21:13:06 +0100

    Updated base project for Rails 3 Beta 3.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '997f8800163b3ce9aa7bff727b12e55449a2cf6f3d54c07c7020e46f71c536c169c97f24bf2965d1337ed044a0f816911d8d47e2dfdea200cd4628799d943f2b'
+Rails.application.config.secret_token = '884c67a8ae0772bcd2120771d37d81f43a005f9a9ebfd40aa330fa44785f9458dfa1522f2ed307e6d493fe943cb6e9da490c51ad6b693594b2fe427ea89d0007'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key    => '_remark_session',
-  :secret => '7b9e4c1c1fe50d1c10cd4e6a82e19cf1182f5f4669910c42a3dada2b3836a6c1586e72702e69eb267e0f9f1922cac483661fa2d60b2f5388929ca6d2f2bd5b6a'
-}
+Rails.application.config.session_store :cookie_store, :key => '_Remark_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Rails.application.config.session_store :active_record_store
