32587e3ca9376e817ec770565dce276f436cf99b
Alex Pardoe <digital.pardoe@gmail.com>
2010-04-23 23:10:55 +0100

    Started testing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  PASSWORD = 'testing'
+  
+  before(:each) do
+    @user = User.new
+    @user.username = 'test'
+    @user.password = PASSWORD
+    @user.password_confirmation = 'testing'
+    @user.email = 'test@example.com'
+    @user.name = 'Test User'
+  end
+  
+  it "generates a password salt" do
+    lambda { @user.save! }.should_not raise_error(ActiveRecord::RecordInvalid)
+    @user.password_salt.should_not be nil
+  end
+  
+  it "encrypts the password" do
+    lambda { @user.save! }.should_not raise_error(ActiveRecord::RecordInvalid)
+    @user.crypted_password.should_not be nil
+    @user.crypted_password.should_not == PASSWORD
+  end
 end
