83d876b157904da4475d69e61d76805e9057073a
Alex Pardoe <digital.pardoe@gmail.com>
2010-05-03 22:28:51 +0100

    More development of comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ActiveRecord::Base
-  validates_presence_of :name, :email, :if => Proc.new { |comment| !comment.user }
+  validates_presence_of :name, :email, :unless => :assigned_user?
   validates_presence_of :user, :if => Proc.new { |comment| !comment.name && !comment.email }
   validates_presence_of :body, :article, :uuid
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :unless => :assigned_user?
   validates_format_of :website, :with => /^$|^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/i
   
   attr_protected :uuid
@@ -17,6 +17,10 @@ class Comment < ActiveRecord::Base
   scope :ham, where(:spam => false)
   
   private
+  def assigned_user?
+    self.user != nil
+  end
+  
   def generate_uuid
     self.uuid = UUIDTools::UUID.timestamp_create.to_s
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,68 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @role = Role.new
+    @role.name = 'user'
+    @role.human_readable = 'Website User'
+    @role.save!
+    
+    @user = User.new
+    @user.username = 'test'
+    @user.password = PASSWORD
+    @user.password_confirmation = PASSWORD
+    @user.email = 'test@example.com'
+    @user.website = 'http://example.com/'
+    @user.save!
+    
+    @article = Article.new
+    @article.title = 'This Is A Test Title'
+    @article.body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas convallis convallis augue, a fringilla risus faucibus nec. Maecenas at nunc augue. Duis blandit posuere nisl a feugiat. Mauris porttitor justo consectetur tellus sodales eu aliquet enim lobortis. Curabitur urna augue, cursus ac posuere quis, pretium vel lectus. Etiam sit amet tortor vitae magna pellentesque eleifend ut non risus. Sed quis magna neque, sit amet interdum leo. In eleifend justo sagittis libero molestie et scelerisque ipsum tristique. Donec scelerisque venenatis mi, vitae egestas nisl luctus eget. Aliquam erat volutpat. Suspendisse potenti. Fusce at leo feugiat massa elementum ornare. Aenean aliquet leo vel lacus auctor ac fermentum nisi suscipit. Morbi mollis orci tortor. Morbi id viverra magna. Aenean volutpat tristique lorem. Ut scelerisque placerat libero rutrum molestie. Quisque scelerisque imperdiet nisl sit amet blandit. Proin lorem purus, sollicitudin non rutrum eget, varius at justo. Etiam ac velit vitae nisl tincidunt consequat eget vel libero.\n\nSed pulvinar, felis eget lobortis facilisis, nisl sapien congue lacus, sed viverra elit est non nibh. Cras rutrum eros et leo fringilla egestas. Etiam congue mollis lacinia. Fusce purus nisl, auctor non placerat pulvinar, auctor et purus. Curabitur ornare porttitor nibh, vel interdum ante convallis vel. Praesent in ante turpis. Nullam viverra enim a tellus auctor imperdiet. Nam pharetra dignissim metus eu facilisis. Nunc id elementum erat. Nulla varius, est id fringilla commodo, dolor metus consequat eros, vel tincidunt ante enim sed mauris. Pellentesque vel velit sed tortor vulputate auctor. Fusce at arcu turpis."
+    @article.user = @user
+    @article.save!
+    
+    @comment = Comment.new
+    @comment.name = 'Person'
+    @comment.email = @user.email
+    @comment.body = "This is a test comment body.\nIt's even got a line break."
+    @comment.article = @article
+  end
+  
+  it "stores a valid comment" do
+    lambda { @comment.save! }.should_not raise_error
+  end
+  
+  it "doesn't allow resetting of uuid" do
+    @comment.save!
+    uuid = @comment.uuid
+    comment = Comment.find(@comment.id)
+    comment.uuid = 'random-string'
+    comment.save!
+    Comment.find(@comment.id).uuid.should == uuid
+  end
+  
+  describe "validation:" do
+    it "shouldn't validate due to missing body" do
+      @comment.body = nil
+      lambda { @comment.save! }.should raise_error(ActiveRecord::RecordInvalid)
+    end
+    
+    it "shouldn't validate due to missing article" do
+      @comment.article = nil
+      lambda { @comment.save! }.should raise_error(ActiveRecord::RecordInvalid)
+    end
+    
+    it "shouldn't validate due to missing user (due to missing email & name)" do
+      @comment.name = nil
+      @comment.email = nil
+      lambda { @comment.save! }.should raise_error(ActiveRecord::RecordInvalid)
+    end
+    
+    it "should validate event with missing email & name because it has a user" do
+      @comment.name = nil
+      @comment.email = nil
+      @comment.user = @user
+      lambda { @comment.save! }.should_not raise_error
+    end
+  end
 end
