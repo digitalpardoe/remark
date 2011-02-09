87ff206bce6f6469d52c5201feffeecf84452592
Alex Pardoe <digital.pardoe@gmail.com>
2011-02-09 13:57:39 +0000

    View tests currently unnecessary as views are (currently) fluid.

--- spec/views/admin/documents/new.html.erb_spec.rb
+++ spec/views/admin/documents/new.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe "documents/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe "documents/new.html.erb" do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
