a4c9100965da1e9a53bca894b4f11f38007d34e1
Alex Pardoe <digital.pardoe@gmail.com>
2011-02-09 19:37:57 +0000

    Start using the blog title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def tracking
     Setting.application.value(:tracking) unless Rails.env != 'production'
   end
+  
+  def name
+    Setting.application.value(:name)
+  end
 end
