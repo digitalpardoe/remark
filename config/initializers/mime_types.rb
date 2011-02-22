f526ec88c64d689586ac9472ca0703c0d9bab86c
Alex Pardoe <digital.pardoe@gmail.com>
2011-02-22 10:11:20 +0000

    Improved MIME registering at application startup.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -7,7 +7,7 @@
 begin
   registered_types = []
   Document.all.each do |document|
-    if !document.extension.blank? && !document.content_type.blank? && !registered_types.include?(document.extension)
+    if !document.extension.blank? && !document.content_type.blank? && !registered_types.include?(document.extension) && !Mime::Type.lookup_by_extension(document.extension)
       Mime::Type.register document.content_type, document.extension.to_sym
       registered_types << document.extension
     end
