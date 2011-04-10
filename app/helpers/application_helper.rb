16c61621300b0604b51f3d05fe6d28b0fc3cb2af
Alex Pardoe <digital.pardoe@gmail.com>
2011-04-10 19:05:05 +0100

    Text filtering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,34 @@ module ApplicationHelper
     Setting.application.value(:name)
   end
   
+  def subtitle
+    Setting.application.value(:subtitle)
+  end
+  
+  def title(title)
+		content_for(:title) { title }
+	end
+  
+  def navigation
+    pages = Page.published.visible.select(:title).select(:permalink).all
+    content_tag :ul do
+      "".tap do |list|
+        pages.each do |page|
+          list << content_tag(:li, link_to(page.title, page_path(:permalink => page.permalink)))
+        end
+      end.html_safe
+    end
+  end
+  
   def article_path(article)
     super :day => article.published_at.strftime("%d"), :month => article.published_at.strftime("%m"), :year => article.published_at.strftime("%Y"), :permalink => article.permalink
   end
   
   def article_url(article)
     "http://" + Setting.application.value(:url) + article_path(article)
+  end
+  
+  def text_filter(entity, text_method)
+    (TEXT_FILTERS.select { |filter| filter[:name] == entity.text_filter }).first[:method].call(entity.send(text_method)).html_safe
   end
 end
--- config/initializers/registry.rb
+++ config/initializers/registry.rb
@@ -1,7 +1,7 @@
 IDENTIFIER = "com.github.digitalpardoe.remark"
 TEXT_FILTERS = [
-  {:name => 'html', :human_readable => 'HTML'},
-  {:name => 'markdown', :human_readable => 'Markdown'},
-  {:name => 'textile', :human_readable => 'Textile'}
+  {:name => 'html', :human_readable => 'HTML', :method => lambda { |text| text }},
+  {:name => 'markdown', :human_readable => 'Markdown', :method => lambda { |text| BlueCloth.new(text).to_html }},
+  {:name => 'textile', :human_readable => 'Textile', :method => lambda { |text| RedCloth.new(text).to_html }}
 ]
 DEFAULT_TEXT_FILTER = TEXT_FILTERS[1][:name]
