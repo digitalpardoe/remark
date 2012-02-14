513e70470b273bb41a172b8b47d481276a10394e
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-14 20:17:27 +0000

    Form styling.

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -5,22 +5,24 @@ module Admin::SettingsHelper
       option = setting.field_type.split('#').last
     end
     
+    classes = ["input-xlarge"]
+    
     case field_type
       when "text_area"
-        form.text_area(setting.id, :value => setting.value, :class => 'normal')
+        form.text_area(setting.id, :value => setting.value, :class => classes, :rows => 10)
       when "url_field"
-        form.url_field(setting.id, :value => setting.value)
+        form.url_field(setting.id, :value => setting.value, :class => classes)
       when "time_zone"
-        select_tag("setting[#{setting.id}]", time_zone_options_for_select(setting.value))
+        select_tag("setting[#{setting.id}]", time_zone_options_for_select(setting.value), :class => classes)
       when "collection_select"
         case option
           when "model"
-            select_tag("setting[#{setting.id}]", options_from_collection_for_select(Object.const_get(setting.key.camelize.to_sym).all, :id, :human_readable, setting.value))
+            select_tag("setting[#{setting.id}]", options_from_collection_for_select(Object.const_get(setting.key.camelize.to_sym).all, :id, :human_readable, setting.value), :class => classes)
           when "constant"
-            form.select setting.id, Object.const_get(setting.key.pluralize.upcase.to_sym).collect { |tf| [truncate(tf[:human_readable]), tf[:name]] }, { :selected => setting.value }
+            form.select setting.id, Object.const_get(setting.key.pluralize.upcase.to_sym).collect { |tf| [truncate(tf[:human_readable]), tf[:name]] }, { :selected => setting.value }, :class => classes
         end
       else
-        form.text_field(setting.id, :value => setting.value)
+        form.text_field(setting.id, :value => setting.value, :class => classes)
     end
   end
 end
