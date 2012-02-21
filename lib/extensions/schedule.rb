f37558f1fa7e2da44f0460f7de3078aaea8a90bd
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-21 15:25:05 +0000

    Refine scheduling a little.

--- lib/extensions/schedule.rb
+++ lib/extensions/schedule.rb
@@ -6,7 +6,7 @@ module Schedule
     SCHEDULER.at self.published_at, :tags => tag do
       # This needs to be made more intelligent (obviously)
       Rails.cache.clear
-    end
+    end unless self.published_at <= Time.now
   end
 
   def unschedule
@@ -21,6 +21,8 @@ module Schedule
   def perform_unschedule(tag)
     existing_jobs = SCHEDULER.find_by_tag(tag)
 
+    # Make sure we don't duplicate jobs, last thing we need is the
+    # cache getting cleared all the time
     if (existing_jobs.length > 0)
       existing_jobs.each do |job|
         job.unschedule
