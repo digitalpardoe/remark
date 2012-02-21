49dc15d1dfba7a8817a9d7b7041a88fcc51ac4b4
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-21 15:51:53 +0000

    More unnecessary.

--- lib/extensions/schedule.rb
+++ lib/extensions/schedule.rb
@@ -1,30 +1,22 @@
 module Schedule
   def schedule
-    tag = generate_tag
-    perform_unschedule(tag)
-
-    SCHEDULER.at self.published_at, :tags => tag do
+    unschedule
+    SCHEDULER.at self.published_at, :tags => generate_tag do
       # This needs to be made more intelligent (obviously)
       Rails.cache.clear
     end unless self.published_at <= Time.now
   end
 
   def unschedule
-    perform_unschedule(generate_tag)
+    # Make sure we don't duplicate jobs, last thing we need is the
+    # cache getting cleared all the time
+    SCHEDULER.find_by_tag(generate_tag).each do |job|
+      job.unschedule
+    end
   end
 
   private
   def generate_tag
     "#{self.class.name.downcase!}_#{self.id}"
-  end
-
-  def perform_unschedule(tag)
-    existing_jobs = SCHEDULER.find_by_tag(tag)
-
-    # Make sure we don't duplicate jobs, last thing we need is the
-    # cache getting cleared all the time
-    existing_jobs.each do |job|
-     job.unschedule
-    end
   end
 end
