4547576d6a7e9507f5096a311ee1948a24e8e125
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-21 22:08:07 +0000

    Cutting more scheduling.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,1 +1,13 @@
 SCHEDULER = Rufus::Scheduler.start_new
+
+Article.where(:draft => false).where('published_at > ?', Time.now).each do |article|
+  SCHEDULER.at article.published_at, :tags => Schedule::Utils.generate_tag(article) do
+    Rails.cache.clear
+  end
+end
+
+Page.where(:draft => false).where('published_at > ?', Time.now).each do |page|
+  SCHEDULER.at page.published_at, :tags => Schedule::Utils.generate_tag(page) do
+    Rails.cache.clear
+  end
+end
--- lib/extensions/schedule.rb
+++ lib/extensions/schedule.rb
@@ -1,7 +1,7 @@
 module Schedule
   def schedule
     unschedule
-    SCHEDULER.at self.published_at, :tags => generate_tag do
+    SCHEDULER.at self.published_at, :tags => Utils.generate_tag(self) do
       # This needs to be made more intelligent (obviously)
       Rails.cache.clear
     end unless self.published_at <= Time.now || self.draft
@@ -10,13 +10,16 @@ module Schedule
   def unschedule
     # Make sure we don't duplicate jobs, last thing we need is the
     # cache getting cleared all the time
-    SCHEDULER.find_by_tag(generate_tag).each do |job|
+    SCHEDULER.find_by_tag(Utils.generate_tag(self)).each do |job|
       job.unschedule
     end
   end
-
-  private
-  def generate_tag
-    "#{self.class.name.downcase!}_#{self.id}"
+  
+  module Utils
+    class << self
+      def generate_tag(object)
+        "#{object.class.name.downcase!}_#{object.id}"
+      end
+    end
   end
 end
