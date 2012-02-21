f631e2c966b072c9c884698850b8fa262af307bc
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-21 12:17:42 +0000

    That was old (and a good example of why you should _always_ put something in a 'rescue' block.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -7,14 +7,3 @@
 Mime::Type.register 'image/gif', :gif
 Mime::Type.register 'application/x-shockwave-flash', :swf
 Mime::Type.register 'image/png', :png
-
-begin
-  registered_types = []
-  Document.all.each do |document|
-    if !document.extension.blank? && !document.content_type.blank? && !registered_types.include?(document.extension) && !Mime::Type.lookup_by_extension(document.extension)
-      Mime::Type.register document.content_type, document.extension.to_sym
-      registered_types << document.extension
-    end
-  end
-rescue
-end
