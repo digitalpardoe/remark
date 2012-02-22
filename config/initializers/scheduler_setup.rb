f8bd17bbc78dc516f07d2584039fbcdb3737eec4
Alex Pardoe <digital.pardoe@gmail.com>
2012-02-22 13:10:22 +0000

    Time zones.

--- config/initializers/scheduler_setup.rb
+++ config/initializers/scheduler_setup.rb
@@ -1,5 +1,9 @@
 SCHEDULER = Rufus::Scheduler.start_new
 
+# Set up any jobs that may already exists, make sure we
+# take the user's time zone setting into account though
+Time.zone = Setting.application.value(:time_zone)
+
 Article.unpublished.each do |article|
   article.schedule
 end
