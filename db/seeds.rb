4168d511c6c3961e701957623195507a26366907
Alex Pardoe <digital.pardoe@gmail.com>
2012-11-19 10:47:17 +0000

    Updated seeds.rb so it works.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,22 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 [
-  {:key => 'name', :value => 'Remark', :hidden => false, :human_readable => "Blog Name", :required => true},
-  {:key => 'subtitle', :value => 'A Rails Blogging Engine', :hidden => false, :human_readable => "Blog Subtitle", :required => true},
-  {:key => 'url', :value => 'http://example.com/', :hidden => false, :human_readable => "Blog URL", :required => true, :field_type => "url_field"},
-  {:key => 'tracking', :hidden => false, :human_readable => "Tracking Code", :required => false, :field_type => "text_area"},
-  {:key => 'text_filter', :value => DEFAULT_TEXT_FILTER, :hidden => false, :human_readable => "Default Text Filter", :required => true, :field_type => "collection_select#constant"},
-  {:key => 'disqus_shortname', :hidden => false, :human_readable => "DISQUS Shortname", :required => false},
-  {:key => 'feedburner_stub', :hidden => false, :human_readable => "FeedBurner Stub", :required => false},
-  {:key => 'per_page', :value => 5, :hidden => false, :human_readable => "Articles Per Page", :required => true},
-  {:key => 'time_zone', :value => DEFAULT_TIME_ZONE, :hidden => false, :human_readable => "Time Zone", :required => true, :field_type => "time_zone"}
+  {:key => 'name', :value => 'Remark', :human_readable => "Blog Name", :required => true},
+  {:key => 'subtitle', :value => 'A Rails Blogging Engine', :human_readable => "Blog Subtitle", :required => true},
+  {:key => 'url', :value => 'http://example.com/', :human_readable => "Blog URL", :required => true, :field_type => "url_field"},
+  {:key => 'tracking', :human_readable => "Tracking Code", :required => false, :field_type => "text_area"},
+  {:key => 'text_filter', :value => DEFAULT_TEXT_FILTER, :human_readable => "Default Text Filter", :required => true, :field_type => "collection_select#constant"},
+  {:key => 'disqus_shortname', :human_readable => "DISQUS Shortname", :required => false},
+  {:key => 'feedburner_stub', :human_readable => "FeedBurner Stub", :required => false},
+  {:key => 'per_page', :value => 5, :human_readable => "Articles Per Page", :required => true},
+  {:key => 'time_zone', :value => DEFAULT_TIME_ZONE, :human_readable => "Time Zone", :required => true, :field_type => "time_zone"}
 ].each do |setting|
   Setting.create!( setting.merge( { :resource => IDENTIFIER } ) )
+end
+
+Setting.all.each do |setting|
+  setting.hidden = false
+  setting.save!
 end
 
 [
