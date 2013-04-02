e41ecafc206cc14c2571a9191d46280abff2545b
Alex Pardoe <digital.pardoe@gmail.com>
2013-04-02 12:00:06 +0100

    Removing S3 support.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,13 +1,5 @@
 class Document < ActiveRecord::Base
-  if S3_ENABLED
-    has_attached_file :paperclip,
-      :storage => :s3,
-      :s3_credentials => { :access_key_id => S3_ACCESS_KEY_ID, :secret_access_key => S3_SECRET_ACCESS_KEY },
-      :bucket => S3_BUCKET,
-      :path => "remark/:attachment/:id/:style/:filename"
-  else
-    has_attached_file :paperclip
-  end
+  has_attached_file :paperclip
   
   validates_attachment_presence :paperclip
   
