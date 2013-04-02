e29335ec1d41e3b921728105f503cc089124acfd
Alex Pardoe <digital.pardoe@gmail.com>
2013-04-02 12:24:35 +0100

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328090639) do
+ActiveRecord::Schema.define(:version => 20130402110223) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
