5d81ed3f1bf110c47aadb650f86a26c134f82cc0
Alex Pardoe <digital.pardoe@gmail.com>
2013-05-12 14:02:36 +0100

    Require stylesheet class.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require File.dirname(__FILE__) + '/../../lib/stylesheet.rb'
+
 Remark::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
