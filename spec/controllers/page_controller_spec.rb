ceca484d85c646674763b1551edd728f668f28c0
Alex Pardoe <digital.pardoe@gmail.com>
2013-06-26 10:34:35 +0100

    Added another test.

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe PageController do
-
-#  describe "GET 'show'" do
-#    it "should be successful" do
-#      get 'show'
-#      response.should be_success
-#    end
-#  end
+  before(:each) do
+    FactoryGirl.create(:time_zone)
+    @page = FactoryGirl.create(:page)
+  end
+  
+  describe "GET 'show'" do
+    it "should be successful" do
+      get 'show', :permalink => @page.permalink
+      response.should be_success
+    end
+  end
 
 end
