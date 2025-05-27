a914509f581dd0cb3297c8fcb72b0c08673e62d7
Alex Pardoe <alex@pardoe.io>
2025-05-27 21:18:48 +0100

    A couple more quick tweaks.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20130411095908) do
 
-  create_table "articles", force: true do |t|
+  create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
@@ -29,25 +28,23 @@ ActiveRecord::Schema.define(version: 20130411095908) do
     t.boolean  "link_article"
   end
 
-  create_table "articles_tags", id: false, force: true do |t|
+  create_table "articles_tags", id: false, force: :cascade do |t|
     t.integer "article_id"
     t.integer "tag_id"
+    t.index ["article_id", "tag_id"], name: "index_articles_tags_on_article_id_and_tag_id"
   end
 
-  add_index "articles_tags", ["article_id", "tag_id"], name: "index_articles_tags_on_article_id_and_tag_id"
-
-  create_table "documents", force: true do |t|
+  create_table "documents", force: :cascade do |t|
     t.string   "paperclip_file_name"
     t.integer  "paperclip_file_size"
     t.string   "paperclip_content_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "paperclip_updated_at"
+    t.index ["paperclip_file_name"], name: "index_documents_on_paperclip_file_name_and_extension", unique: true
   end
 
-  add_index "documents", ["paperclip_file_name"], name: "index_documents_on_paperclip_file_name_and_extension", unique: true
-
-  create_table "pages", force: true do |t|
+  create_table "pages", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
@@ -61,16 +58,15 @@ ActiveRecord::Schema.define(version: 20130411095908) do
     t.integer  "sort_order",   default: 0
   end
 
-  create_table "roles", force: true do |t|
+  create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.string   "human_readable"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
-  add_index "roles", ["name"], name: "index_roles_on_name", unique: true
-
-  create_table "settings", force: true do |t|
+  create_table "settings", force: :cascade do |t|
     t.string   "resource"
     t.string   "key"
     t.text     "value",          limit: 255
@@ -80,19 +76,17 @@ ActiveRecord::Schema.define(version: 20130411095908) do
     t.datetime "updated_at"
     t.boolean  "required",                   default: false
     t.string   "field_type"
+    t.index ["resource", "key"], name: "index_settings_on_resource_and_key", unique: true
   end
 
-  add_index "settings", ["resource", "key"], name: "index_settings_on_resource_and_key", unique: true
-
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "crypted_password"
     t.string   "password_salt"
@@ -101,8 +95,7 @@ ActiveRecord::Schema.define(version: 20130411095908) do
     t.datetime "updated_at"
     t.integer  "role_id"
     t.string   "website"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
-
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
